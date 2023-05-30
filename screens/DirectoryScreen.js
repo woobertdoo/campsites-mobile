725291e77fa5ca5e30727a029304f5e3a366b113
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2023-05-29 20:06:19 -0500

    Main Directory Screen

---
+++ screens/DirectoryScreen.js
@@ -0,0 +1,28 @@
+import { FlatList } from 'react-native-gesture-handler';
+import { Avatar, ListItem } from 'react-native-elements';
+
+const DirectoryScreen = ({ campsites }) => {
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem>
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
+
+    return (
+        <FlatList
+            data={campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    );
+};
+
+export default DirectoryScreen;
