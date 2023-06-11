bfd25b685ba8d46f06c8d71cddba23a5bba409b5
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2023-06-11 14:56:02 -0500

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Avatar, Card, Text, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from '../components/LoadingComponent';
 
 const Mission = () => {
     return (
@@ -23,7 +24,32 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
 
     return (
         <ScrollView>
@@ -31,10 +57,13 @@ const AboutScreen = () => {
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.map((partner) => {
+                {partners.partnersArray.map((partner) => {
                     return (
                         <ListItem key={partner.id}>
-                            <Avatar rounded source={partner.image} />
+                            <Avatar
+                                rounded
+                                source={{ uri: baseUrl + partner.image }}
+                            />
                             <ListItem.Content>
                                 <ListItem.Title>{partner.name}</ListItem.Title>
                                 <ListItem.Subtitle>
--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,32 +1,40 @@
 import { FlatList } from 'react-native-gesture-handler';
-import { Avatar, ListItem } from 'react-native-elements';
-import { useState } from 'react';
-import { CAMPSITES } from '../shared/campsites';
+import { Tile } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
 const DirectoryScreen = ({ navigation }) => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
+    const campsites = useSelector((state) => state.campsites);
+
+    if (campsites.isLoading) {
+        return <Loading />;
+    }
+    if (campsites.errMess) {
+        return (
+            <View>
+                <Text>{campsites.errMess}</Text>
+            </View>
+        );
+    }
 
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
-            <ListItem
+            <Tile
                 onPress={() =>
                     navigation.navigate('CampsiteInfo', { campsite })
                 }
-            >
-                <Avatar source={campsite.image} rounded />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
+                title={campsite.name}
+                caption={campsite.description}
+                featured
+                imageSrc={{ uri: baseUrl + campsite.image }}
+            />
         );
     };
 
     return (
         <FlatList
-            data={campsites}
+            data={campsites.campsitesArray}
             renderItem={renderDirectoryItem}
             keyExtractor={(item) => item.id.toString()}
         />
