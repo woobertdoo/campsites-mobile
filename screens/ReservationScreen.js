5211dcc64648fcaeaa6e56e563a36b0088e7ea9a
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2023-06-11 19:24:48 -0500

    Modals

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -6,6 +6,7 @@ import {
     StyleSheet,
     Switch,
     Button,
+    Modal,
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -15,6 +16,14 @@ const ReserverationScreen = () => {
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalender, setShowCalender] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    const resetForm = () => {
+        setCampers(1);
+        setHikeIn(false);
+        setDate(new Date());
+        setShowCalender(false);
+    };
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -31,6 +40,7 @@ const ReserverationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalender(false);
+        setShowModal(!showModal);
     };
 
     return (
@@ -85,6 +95,35 @@ const ReserverationScreen = () => {
                     accessibilityLabel="Tap me to search for available campsites to reserve"
                 />
             </View>
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>
+                        Search Campsite Reservations
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Number of Campers: {campers}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Hike-In? {hikeIn ? 'Yes' : 'No'}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Date: {date.toLocaleDateString('en-US')}
+                    </Text>
+                    <Button
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                        color="#5637DD"
+                        title="Close"
+                    />
+                </View>
+            </Modal>
         </ScrollView>
     );
 };
@@ -103,6 +142,22 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1,
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20,
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#5637DD',
+        textAlign: 'center',
+        color: '#fff',
+        marginBottom: 20,
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10,
     },
 });
 
